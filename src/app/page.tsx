08948a5f96b1ea8080946b55ebede6f5ad3228b7
Golam Mahabub Redoy <hasanhridoymahabub9@gmail.com>
2026-02-22 22:51:24 +0600

    feat: implemented how it works section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Hero } from "@/components/home/hero";
 import { Features } from "@/components/home/features";
+import { HowItWorks } from "@/components/home/how-it-works";
 
 export default function Page() {
   return (
     <main>
       <Hero />
       <Features />
+      <HowItWorks />
     </main>
   );
 }
